4b0171c152f7d505fd5dba716b03fe549a039234
Linkeriyo <55212637+Linkeriyo@users.noreply.github.com>
2021-02-24 13:33:31 +0100

    splash

--- app/src/main/java/com/example/practicamercadomunicipal/SplashActivity.java
+++ app/src/main/java/com/example/practicamercadomunicipal/SplashActivity.java
@@ -1,18 +1,13 @@
 package com.example.practicamercadomunicipal;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
 public class SplashActivity extends AppCompatActivity {
     private final static String TAG = "SplashActivity";
-    private final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
