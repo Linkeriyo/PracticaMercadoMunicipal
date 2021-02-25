9c3d2f644e6dd8b2e17b4033b6365fa98bd8de47
IvanMolina007 <60225409+IvanMolina007@users.noreply.github.com>
2021-02-25 19:37:20 +0100

    Registro de usuarios en la base de datos

--- app/src/main/java/com/example/practicamercadomunicipal/RegisterActivity.java
+++ app/src/main/java/com/example/practicamercadomunicipal/RegisterActivity.java
@@ -1,14 +1,25 @@
 package com.example.practicamercadomunicipal;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.example.practicamercadomunicipal.models.Invoice;
 import com.example.practicamercadomunicipal.models.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -17,8 +28,20 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    String imageUri = "";
+    ImageView imageView;
+    ProgressBar progressBar;
+    private static final int PICK_IMAGE = 1;
+    FirebaseStorage storage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,26 +55,23 @@ public class RegisterActivity extends AppCompatActivity {
         bundle.putString("message", "Full Firebase integration");
         analytics.logEvent("InitScreen", bundle);
 
-        /*Button accept = findViewById(R.id.buttonAcceptCreate);
-        final EditText email = findViewById(R.id.editTextEmailAddressCreate);
-        final EditText password = findViewById(R.id.editTextPasswordCreate);
-        final EditText name = findViewById(R.id.editTextName);
-        final EditText phone = findViewById(R.id.editTextPhone);
-        final EditText desc = findViewById(R.id.editTextDesc);
-
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-
+        TextView accept = findViewById(R.id.textViewCreate);
+        final EditText email = findViewById(R.id.editTextEmailAddressRegister);
+        final EditText password = findViewById(R.id.editTextPasswordRegister);
+        final EditText name = findViewById(R.id.editTextNameRegister);
+        final EditText saldo = findViewById(R.id.editTextBalanceRegister);
+        List<Invoice> invoiceList = new ArrayList<Invoice>();
+        imageView = findViewById(R.id.imageButton);
+        imageView.setOnClickListener(v -> {
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+            startActivityForResult(galleryIntent, PICK_IMAGE);
         });
 
         accept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (email.getText().toString().isEmpty() || password.getText().toString().isEmpty() || name.getText().toString().isEmpty() || phone.getText().toString().isEmpty() || desc.getText().toString().isEmpty()) {
-                    Toast empty = Toast.makeText(getApplicationContext(), "You have left a blank", Toast.LENGTH_SHORT);
+                if (email.getText().toString().isEmpty() || password.getText().toString().isEmpty() || name.getText().toString().isEmpty() || saldo.getText().toString().isEmpty()) {
+                    Toast empty = Toast.makeText(getApplicationContext(), "Dejaste algun campo en blanco", Toast.LENGTH_SHORT);
                     empty.show();
                 } else {
                     final FirebaseAuth auth = FirebaseAuth.getInstance();
@@ -59,12 +79,12 @@ public class RegisterActivity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                Toast correct = Toast.makeText(getApplicationContext(), "User created successfully", Toast.LENGTH_SHORT);
+                                Toast correct = Toast.makeText(getApplicationContext(), "El usuario se creó satisfactoriamente", Toast.LENGTH_SHORT);
                                 correct.show();
-                                newUser(auth.getCurrentUser().getUid(), name.getText().toString(), phone.getText().toString(), desc.getText().toString());
+                                newUser(true, auth.getCurrentUser().getUid(), name.getText().toString(), imageUri, email.getText().toString(), invoiceList, Double.parseDouble(saldo.getText().toString()));
                                 finish();
                             } else {
-                                Toast incorrect = Toast.makeText(getApplicationContext(), "User could not be created, check email and password", Toast.LENGTH_SHORT);
+                                Toast incorrect = Toast.makeText(getApplicationContext(), "El usuario no se pudo crear, revise que la contraseña sea mas de 5 dígitos y el correo este bien escrito", Toast.LENGTH_SHORT);
                                 incorrect.show();
                             }
                         }
@@ -74,10 +94,50 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    public void newUser(String id, String name, String mobile, String desc) {
-        User user = new User(id, name, mobile, desc);
+    public void newUser(boolean admin, String id, String name, String image, String email, List<Invoice> invoiceList, Double saldo) {
+        User user = new User(admin, id, name, image, email, invoiceList, saldo);
 
         DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
-        mDatabase.child("users").child(id).setValue(user);*/
+        mDatabase.child("users").child(id).setValue(user);
+    }
+
+    private void progressBar() {
+        progressBar = findViewById(R.id.new_store_progressbar);
+        progressBar.setProgress(200);
+        progressBar.setVisibility(View.VISIBLE);
+        progressBar.setMax(1000);
+        ObjectAnimator.ofInt(progressBar, "progress", 999)
+                .setDuration(2000)
+                .start();
+    }
+
+    private void putImage(String imageUri) {
+        Glide.with(this)
+                .load(imageUri)
+                .centerCrop()
+                .into(imageView);
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE) {
+            assert data != null;
+            progressBar();
+            Uri uri = data.getData();
+            StorageReference fileReference = storage.getReference("images").child(uri.getLastPathSegment());
+            fileReference.putFile(uri).continueWithTask(task -> {
+                if (!task.isSuccessful()) {
+                    throw Objects.requireNonNull(task.getException());
+                }
+                return fileReference.getDownloadUrl();
+            }).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    imageUri = Objects.requireNonNull(task.getResult()).toString();
+                    putImage(imageUri);
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/example/practicamercadomunicipal/models/User.java
+++ app/src/main/java/com/example/practicamercadomunicipal/models/User.java
@@ -9,4 +9,13 @@ public class User {
     List<Invoice> invoice;
     double balance;
 
+    public User(boolean admin, String userID, String name, String image, String email, List<Invoice> invoice, double balance) {
+        this.admin = admin;
+        this.userID = userID;
+        this.name = name;
+        this.image = image;
+        this.email = email;
+        this.invoice = invoice;
+        this.balance = balance;
+    }
 }
