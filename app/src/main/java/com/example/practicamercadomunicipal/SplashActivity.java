7b94bb35430db57dab0837892bbfd4ff6601786e
Linkeriyo <55212637+Linkeriyo@users.noreply.github.com>
2021-02-26 01:37:43 +0100

    cambio en splashactivity
    cambiada la forma en la que se cargan los datos por errores de sincronización

--- app/src/main/java/com/example/practicamercadomunicipal/SplashActivity.java
+++ app/src/main/java/com/example/practicamercadomunicipal/SplashActivity.java
@@ -4,13 +4,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.practicamercadomunicipal.data.AppData;
 import com.example.practicamercadomunicipal.models.Store;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Logger;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,9 +34,9 @@ public class SplashActivity extends AppCompatActivity {
 
     private void loadData() {
         DatabaseReference storesReference = FirebaseDatabase.getInstance().getReference("stores");
-        storesReference.get().addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                DataSnapshot snapshot = task.getResult();
+        storesReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
                 List<Store> stores = new ArrayList<>();
                 snapshot.getChildren().forEach(child -> {
                     Store store = child.getValue(Store.class);
@@ -40,9 +45,15 @@ public class SplashActivity extends AppCompatActivity {
                     }
                 });
                 AppData.storeList = stores;
+                storesLoaded = true;
+                tryNextActivity();
+                storesReference.removeEventListener(this);
             }
-            storesLoaded = true;
-            tryNextActivity();
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                error.toException().printStackTrace();
+            }
         });
     }
 
